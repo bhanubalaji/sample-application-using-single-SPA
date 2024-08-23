eafe2836f890f0d87061873b2d139e035007a89d
B BhanuBalaji <bezawada_balaji@nextazy.com>
2024-08-23 18:57:05 +0530

    add custom events

--- src/testOrg-root-config.ts
+++ src/testOrg-root-config.ts
@@ -6,15 +6,23 @@ import {
 } from "single-spa-layout";
 import microfrontendLayout from "./microfrontend-layout.html";
 
+// Create routes based on the layout HTML
 const routes = constructRoutes(microfrontendLayout);
+
+// Define applications based on the routes and their names
 const applications = constructApplications({
   routes,
   loadApp({ name }) {
     return System.import(name);
   },
 });
+
+// Create and configure the layout engine
 const layoutEngine = constructLayoutEngine({ routes, applications });
 
+// Register each application
 applications.forEach(registerApplication);
+
+// Activate the layout engine and start single-spa
 layoutEngine.activate();
 start();
